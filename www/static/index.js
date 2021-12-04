96d2d0a2f6aa0d8c7c70bcf7877219f95380333c
Tomek Karwowski <to.karwowski@gmail.com>
2021-12-04 23:35:25 +0100

    initial working echo server

---
+++ www/static/index.js
@@ -0,0 +1,36 @@
+const chat = document.getElementById('chat');
+const send = document.getElementById('send');
+const string1 = document.getElementById('string1');
+const string2 = document.getElementById('string2');
+const uri = 'ws://' + location.host + '/ws';
+const ws = new WebSocket(uri);
+
+console.log("this gets executed");
+
+ws.onopen = function() {
+    chat.innerHTML = '<p><em>Connected!</em></p>';
+};
+
+ws.onmessage = function(msg) {
+    const line = document.createElement('p');
+    const data = JSON.parse(JSON.parse(msg.data)); // TODO: temporary fix, we send double-serialized json
+    console.log(data);
+    line.innerText = `string1: ${data["string1"]}, string2: ${data["string2"]}`;
+    chat.appendChild(line);
+};
+
+ws.onclose = function() {
+    chat.getElementsByTagName('em')[0].innerText = 'Disconnected!';
+};
+
+send.onclick = function() {
+    const msg = {
+        'string1': string1.value,
+        'string2': string2.value,
+    }
+
+    ws.send(JSON.stringify(msg));
+
+    string1.value = '';
+    string2.value = '';
+};
